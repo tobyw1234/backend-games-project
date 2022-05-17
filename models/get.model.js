cce4e4181c945fc56b34317b3fa07af841781b05
tobyw1234 <toby0977@gmail.com>
2022-05-17 18:20:45 +0100

    completed get users

---
+++ models/get.model.js
@@ -0,0 +1,10 @@
+const { del } = require("express/lib/application");
+const req = require("express/lib/request");
+const db = require("../db/connection");
+
+exports.fetchAllUsers = () => {
+    return db.query(`SELECT * FROM users`)
+        .then((users) => {
+        return users.rows
+    })
+};
