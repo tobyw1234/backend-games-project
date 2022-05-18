721f1eb703909a47e7a00d8b40ff6c7fd56d2ded
tobyw1234 <toby0977@gmail.com>
2022-05-18 09:24:18 +0100

    neaten up code a bit

--- app.controller.js
+++ app.controller.js
@@ -1,17 +1,17 @@
-const { fetchCategories, fetchReviewById } = require("./app.model")
+const { fetchCategories, fetchReviewById } = require("./app.model");
 
 
 
 exports.getCategories = (req, res) => {
     fetchCategories().then((categories) => {
-        res.status(200).send({categories})
+        res.status(200).send({ categories });
     })
 }
 
 exports.getReviewById = (req, res, next) => {
-    const review_id = req.params.review_id
+    const review_id = req.params.review_id;
      fetchReviewById(review_id).then((reviews) => {
-        res.status(200).send({reviews})
+         res.status(200).send({ reviews });
      }).catch((err) => {
          next(err);
     })
--- app.model.js
+++ app.model.js
@@ -17,18 +17,17 @@ exports.fetchReviewById = (review_id = 1) => {
 	const isReview_idANum = parseInt(review_id);
 	if (!isReview_idANum) {
 		return Promise.reject({ status: 400, msg: "invalid id" });
-	}
+	};
 	const reviews = db.query(`SELECT * FROM reviews WHERE review_id = $1`, [
 		review_id,
 	]);
-	const commentCount = db.query(`SELECT * FROM comments WHERE review_id = $1`, [review_id]
-	)
+	const commentCount = db.query(`SELECT * FROM comments WHERE review_id = $1`, [review_id]);
 	return Promise.all([reviews, commentCount]).then(([reviews, commentCount]) => {
 		if (!reviews.rows.length) {
 			return Promise.reject({ status: 404, msg: "director not found" });
 		}
 	
-		let reply = reviews.rows[0]
+		let reply = reviews.rows[0];
 		reply.comment_count = commentCount.rows.length;
 		
 		
