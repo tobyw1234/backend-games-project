e187cf1463ec4762deba2d144dbae12a324fa68d
tobyw1234 <toby0977@gmail.com>
2022-05-20 09:26:38 +0100

    deleted comment

--- models/reviews-model.js
+++ models/reviews-model.js
@@ -2,31 +2,6 @@ const { del } = require("express/lib/application");
 const req = require("express/lib/request");
 const db = require("../db/connection");
 
-// exports.fetchReviewById = (review_id = 1) => {
-// 	const isReview_idANum = parseInt(review_id);
-// 	if (!isReview_idANum) {
-// 		return Promise.reject({ status: 400, msg: "invalid id" });
-// 	}
-// 	const reviews = db.query(`SELECT * FROM reviews WHERE review_id = $1`, [
-// 		review_id,
-// 	]);
-// 	const commentCount = db.query(`SELECT * FROM comments WHERE review_id = $1`, [
-// 		review_id,
-// 	]);
-// 	return Promise.all([reviews, commentCount]).then(
-// 		([reviews, commentCount]) => {
-// 			if (!reviews.rows.length) {
-// 				return Promise.reject({ status: 404, msg: "director not found" });
-// 			}
-
-// 			let reply = reviews.rows[0];
-// 			reply.comment_count = commentCount.rows.length;
-
-// 			return reply;
-// 		}
-// 	);
-// };
-
 exports.fetchReviewById = (review_id = 1) => {
 	const isReview_idANum = parseInt(review_id);
 
@@ -48,8 +23,6 @@ exports.fetchReviewById = (review_id = 1) => {
 	}
 }
 	
-		
-
 	exports.checkReviewExists = (review_id) => {
 
 		return db.query(`SELECT * FROM reviews WHERE review_id = $1`, [review_id])
@@ -83,8 +56,6 @@ exports.fetchReviewById = (review_id = 1) => {
 			return updateReview.rows[0];
 		});
 	};
-
-
 
 	exports.fetchAllReviews = () => {
 		return db
