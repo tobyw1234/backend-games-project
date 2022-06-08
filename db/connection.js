4f8989c6cfb877ce03499bd7b102c27b95a72028
tobyw1234 <toby0977@gmail.com>
2022-06-08 16:08:01 +0100

    troubleshooting

--- db/connection.js
+++ db/connection.js
@@ -1,18 +1,22 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
-
+console.log(process.env)
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
+
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {};
 
 if (!process.env.PGDATABASE) {
   throw new Error('PGDATABASE not set');
 }
 
-const config = ENV === "production" ? {
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-} : {};
 
 
 module.exports = new Pool(config);
